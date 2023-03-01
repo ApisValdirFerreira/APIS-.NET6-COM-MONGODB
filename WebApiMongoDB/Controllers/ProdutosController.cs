e9c023bd1e746d1bea5289858e347b5d76d27329
rafael sanchez port <rafysanchez@hotmail.com>
2023-03-01 13:18:15 -0300

    ajustes controller

--- WebApiMongoDB/Controllers/ProdutosController.cs
+++ WebApiMongoDB/Controllers/ProdutosController.cs
@@ -27,5 +27,48 @@ namespace WebApiMongoDB.Controllers
 
             return produto;
         }
+
+        [HttpGet("{id}")]
+        public async Task<Produto> GetById(string id)
+        {
+            return await _produtoServices.GetAsync(id);
+
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(string id, [FromBody] Produto produto)
+        {
+            try
+            {
+                await _produtoServices.UpdateAsync(id, produto);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("dados inválidos");
+            }
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult>  Delete(string id)
+        {
+            try
+            {
+                await _produtoServices.RemoveAsync(id);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest("dados inválidos");
+            }
+          
+        }
+
     }
+
+}
 }
